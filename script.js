615ba45f9be715903178fda2a177d597f6eb0448
tpett20 <thomaspettersen20@gmail.com>
2024-04-23 15:52:53 -0400

    eliminate submission form, remove string boxes at game init

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const enteredStyle = ["col-2", "col-sm-1", "text-primary-emphasis", "bg-primary-
 
 /* ~~~~ Variables ~~~~ */
 let string
-let stringDisplay
+let userEntry
 let idx
 let newChar
 let score
@@ -16,32 +16,36 @@ const stringBoxSection = document.querySelector("#string-boxes")
 const lastCharEl = document.querySelector("#last-char")
 const msgEl = document.querySelector("#message")
 const scoreEl = document.querySelector("#score")
-const textField = document.querySelector("#text-field")
-const submitBtn = document.querySelector("#submit-btn")
 
 /* ~~~~ Event Listeners ~~~~ */
 document.addEventListener("keydown", handleKeyPress)
-submitBtn.addEventListener('click', handleSubmit)
 
 /* ~~~~ Functions ~~~~ */
 init()
 
 function init() {
     string = ""
-    stringDisplay = []
+    userEntry = []
     score = 0
-    message = "Type the Whole String Below"
+    message = "Enter the Whole String"
+    removeStringBoxes()
     startNewRound()
     render()
+}
+
+function removeStringBoxes() {
+    const boxLen = stringBoxSection.children.length
+    for (let i = 0; i < boxLen; i++) {
+        stringBoxSection.removeChild(stringBoxSection.firstElementChild)
+    }
 }
 
 function startNewRound() {
     newChar = getRandomChar()
     idx = 0
     string += newChar
-    clearStringBoxes()
+    resetUserEntry()
     addStringBox()
-    refreshStringDisplay()
     score = string.length - 1
 }
 
@@ -51,60 +55,38 @@ function getRandomChar() {
     return num.toString()
 }
 
-function clearStringBoxes() {
-    const boxes = stringBoxSection.children
-    for (const box of boxes) {
-        box.innerText = "_"
-        box.className = ""
-        box.classList.add(...blankStyle)
-    }
+function resetUserEntry() {
+    userEntry.push("")
+    userEntry.fill("_")
 }
 
 function addStringBox() {
     const box = document.createElement("div")
-    box.innerText = newChar
-    box.classList.add(...blankStyle)
-    box.classList.add("text-secondary")
     stringBoxSection.appendChild(box)
 }
 
-function refreshStringDisplay() {
-    stringDisplay = new Array(string.length - 1).fill("_")
-}
-
 function render() {
-    // renderStringBoxes()
-    renderStringDisplay()
-    renderLastChar()
+    renderStringBoxes()
     renderMessage()
     renderScore()
 }
 
 function renderStringBoxes() {
     const boxes = stringBoxSection.children
-    for (let i = 0; i < boxes.length - 1; i++) {
+    for (let i = 0; i < boxes.length; i++) {
         const box = boxes[i]
-        box.innerText = "_"
+        const value = userEntry[i]
+        box.innerText = value
         box.className = ""
-        box.classList.add(...blankStyle)
-    }
-}
-
-function renderStringDisplay() {
-    if (idx === 0) {
-        lastCharEl.classList.remove("hidden")
-    }
-    stringDisplayEl.innerText = stringDisplay.join(" ")
-    if (string.length > 1) {
-        stringDisplayEl.innerText += " "
-    }
-}
-
-function renderLastChar() {
-    if (idx <= string.length - 1) {
-        lastCharEl.innerText = newChar
-    } else {
-        lastCharEl.classList.add("hidden")
+        if (value === "_") {
+            box.classList.add(...blankStyle)
+            if (i === boxes.length - 1) {
+                box.classList.add("text-secondary")
+                box.innerText = newChar
+            }
+        } else {
+            box.classList.add(...enteredStyle)
+        }
     }
 }
 
@@ -120,34 +102,32 @@ function handleKeyPress(evt) {
     const key = evt.key
     const code = key.charCodeAt()
     if (key === "Backspace") {
-        updateStringDisplay("_", --idx)
+        updateUserEntry("_", --idx)
     } else if (code >= 48 && code <= 57) {
-        updateStringDisplay(key, idx++)
+        updateUserEntry(key, idx++)
+    }
+    if (idx === string.length) {
+        handleSubmit()
     }
     render()
 }
 
-function updateStringDisplay(value, index) {
-    stringDisplay[index] = value
-    stringBoxSection.children.item(index).innerText = value
+function updateUserEntry(value, index) {
+    userEntry[index] = value
 }
 
-function handleSubmit(evt) {
-    // prevent the page from automatically refreshing
-    evt.preventDefault()
+function handleSubmit() {
     const responseIsCorrect = checkSubmission()
     if (responseIsCorrect === true) {
         startNewRound()
     } else {
         gameOver()
     }
-    // reset text field
-    textField.value = ""
     render()
 }
 
 function checkSubmission() {
-    const submission = textField.value
+    const submission = userEntry.join("")
     return submission === string
 }
 
